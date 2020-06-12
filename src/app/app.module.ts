562cef4a02e6b5eeb9fc8cd04390b839a5f3943d
superdev807 <charles.ferria21@gmail.com>
2020-06-12 00:39:41 -0400

    logo display on the banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, BannerComponent],
+  imports: [BrowserModule, RouterModule.forRoot(ROUTES, { useHash: true })],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
